2a3f76c7b99852b8d635fd8b1d184d482970690c
shivamsharma009 <shivamsharma0501@gmail.com>
2021-08-10 19:36:42 +0530

    Git repos ui integrate with api

--- src/app/components/github-repos/github-repos.component.ts
+++ src/app/components/github-repos/github-repos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-github-repos',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-repos.component.css']
 })
 export class GithubReposComponent implements OnInit {
+
+  @Input() githubRepos:any[];
 
   constructor() { }
 
